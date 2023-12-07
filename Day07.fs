28a65f42902f258ff5f1524cb4f83f7c2183ae25
andi0b <andix25@gmail.com>
2023-12-07 23:47:50 +0100

    Day 7

--- Day07.fs
+++ Day07.fs
@@ -1,6 +1,5 @@
 ﻿module aoc23.Day07
 
-
 type Card = Card of char
 type Hand = Hand of Card list
 
@@ -27,38 +26,44 @@ module Card =
 
 module Hand =
     let typeScore (Hand cards) =
-        let counts = cards |> List.countBy id |> List.map snd |> List.sortByDescending id
+        cards
+        |> List.countBy id
+        |> List.map snd
+        |> List.sortByDescending id
+        |> function
+            | [ 5 ] -> 6 // Five of a kind
+            | 4 :: _ -> 5 // Four of a kind
+            | [ 3; 2 ] -> 4 // Full house
+            | 3 :: _ -> 3 // Three of a kind
+            | 2 :: 2 :: _ -> 2 // Two pairs
+            | 2 :: _ -> 1 // One pair
+            | [ 1; 1; 1; 1; 1 ] -> 0 // High card
+            | _ -> failwith "impossible combination"
 
-        match counts with
-        | 5 :: _ -> 6 // Five of a kind
-        | 4 :: _ -> 5 // Four of a kind
-        | 3 :: 2 :: _ -> 4 // Full house
-        | 3 :: _ -> 3 // Three of a kind
-        | 2 :: 2 :: _ -> 2 // Two pairs
-        | 2 :: _ -> 1 // One pair
-        | _ -> 0 // High card
-
-    // calculate a comparable score by taking the first-most count and multiply by 10, then adding the second most count
-    // five of a kind = 50; four of a kind = 41; full house = 32; three of a kind = 31; two pairs = 22; one pair = 21; high card = 11
+    /// calculate a comparable score by taking the first-most count and multiply by 10, then adding the second most count.
+    /// five of a kind = 50; four of a kind = 41; full house = 32; three of a kind = 31; two pairs = 22; one pair = 21; high card = 11
     let typeScoreJoker (Hand cards) =
         let jokers, others = cards |> List.partition ((=) (Card 'J'))
-        let counts = others |> List.countBy id |> List.map snd |> List.sortByDescending id
 
-        match counts with
-        | fst' :: snd' :: _ -> (fst' + jokers.Length) * 10 + snd'
-        | _ -> 50 // 1 or 0 non-joker cards always make a full house
+        others
+        |> List.countBy id
+        |> List.map snd
+        |> List.sortByDescending id
+        |> function
+            | fst' :: snd' :: _ -> (fst' + jokers.Length) * 10 + snd'
+            | _ -> 50 // only one type of card always makes a full house
 
-    // treat card values as 4 bit numbers and just shift/add them to form a 20 bit long comparable score
+    /// treat card values as 4 bit ints and shift/add them to form a 20 bit comparable score
     let cardsValueScore (Hand cards) =
         (0, cards) ||> Seq.fold (fun score card -> (score <<< 4) + (card |> Card.value))
 
     let cardsValueScoreJoker (Hand cards) =
-        let replacer =
-            function
+        cards
+        |> List.map (function
             | Card 'J' -> Card '0'
-            | c -> c
-
-        cards |> List.map replacer |> Hand |> cardsValueScore
+            | c -> c)
+        |> Hand
+        |> cardsValueScore
 
 let solve valueScore typeScore input =
     input
@@ -68,11 +73,8 @@ let solve valueScore typeScore input =
     |> Seq.mapi (fun i (_, bid) -> bid * (i + 1))
     |> Seq.sum
 
-let part1 input =
-    input |> solve Hand.cardsValueScore Hand.typeScore
-
-let part2 input =
-    input |> solve Hand.cardsValueScoreJoker Hand.typeScoreJoker
+let part1 = solve Hand.cardsValueScore Hand.typeScore
+let part2 = solve Hand.cardsValueScoreJoker Hand.typeScoreJoker
 
 let run = runReadAllLines part1 part2
 
