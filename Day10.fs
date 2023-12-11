93f2a32edb8f5d6473303f3ab1360f9eda61e37c
andi0b <andix25@gmail.com>
2023-12-11 12:14:50 +0100

    Day 10.2

--- Day10.fs
+++ Day10.fs
@@ -9,83 +9,181 @@ type Pipe = Pipe of Vector<float> * Vector<float>
 type GridItem =
     | PipeItem of Pipe
     | StartItem
-    | Unknown
+    | EmptyItem
+    | UnknownItem
 
 type Cursor =
     { direction: Vector<float>
-      location: Vector<float> }
+      position: Vector<float> }
 
 module Grid =
-    let charAt (point: Vector<float>) (lines: string array) =
-        match point |> Vector.toArray with
-        | [| x; y |] -> lines[int y][int x]
-        | _ -> failwith "vector length is not equal to 2"
-
     let findStart (lines: string array) =
         let y = lines |> Array.findIndex (_.Contains('S'))
         let x = lines[y].IndexOf('S')
         vector [| float x; float y |]
 
+    let tryCharAt (point: Vector<float>) (lines: string array) =
+        match point |> Vector.raw with
+        | [| x; y |] ->
+            if y >= 0 && y < lines.Length && x >= 0 && x < lines[int y].Length then
+                Some(lines[int y][int x])
+            else
+                None
+        | _ -> failwith "vector length is not equal to 2"
+
+    let tryItemAt point lines =
+        lines
+        |> tryCharAt point
+        |> Option.map (function
+            | '|' -> Pipe(vector [ 0.0; 1.0 ], vector [ 0.0; -1.0 ]) |> PipeItem
+            | '-' -> Pipe(vector [ 1.0; 0.0 ], vector [ -1.0; 0.0 ]) |> PipeItem
+            | 'J' -> Pipe(vector [ 1.0; 0.0 ], vector [ 0.0; 1.0 ]) |> PipeItem
+            | '7' -> Pipe(vector [ 1.0; 0.0 ], vector [ 0.0; -1.0 ]) |> PipeItem
+            | 'F' -> Pipe(vector [ -1.0; 0.0 ], vector [ 0.0; -1.0 ]) |> PipeItem
+            | 'L' -> Pipe(vector [ -1.0; 0.0 ], vector [ 0.0; 1.0 ]) |> PipeItem
+            | 'S' -> StartItem
+            | '.' -> EmptyItem
+            | _ -> UnknownItem)
+
+    let itemAt point lines = tryItemAt point lines |> Option.get
+    let charAt point lines = tryCharAt point lines |> Option.get
+
+    let allDirections =
+        ((1.0, 0.0), Seq.replicate 4 ())
+        ||> Seq.scan (fun (x, y) _ -> (-y, x))
+        |> Seq.map (fun (x, y) -> vector [| x; y |])
+
+
+module Cursor =
+    let forward cursor =
+        { cursor with
+            position = cursor.position + cursor.direction }
+
+    let changeDirection newDirection cursor =
+        { cursor with direction = newDirection }
+
+    let leftRightPositions cursor =
+        match cursor.direction |> Vector.raw with
+        | [| x; y |] -> (vector [| y; -x |], vector [| -y; x |]) |> TupleEx.map ((+) cursor.position)
+        | _ -> failwith "vector length is not equal to 2"
+
+    let forwardPosition cursor = cursor.position + cursor.direction
 
 module Pipe =
-    let fromChar =
-        function
-        | '|' -> Pipe(vector [ 0.0; 1.0 ], vector [ 0.0; -1.0 ]) |> PipeItem
-        | '-' -> Pipe(vector [ 1.0; 0.0 ], vector [ -1.0; 0.0 ]) |> PipeItem
-        | 'J' -> Pipe(vector [ 1.0; 0.0 ], vector [ 0.0; 1.0 ]) |> PipeItem
-        | '7' -> Pipe(vector [ 1.0; 0.0 ], vector [ 0.0; -1.0 ]) |> PipeItem
-        | 'F' -> Pipe(vector [ -1.0; 0.0 ], vector [ 0.0; -1.0 ]) |> PipeItem
-        | 'L' -> Pipe(vector [ -1.0; 0.0 ], vector [ 0.0; 1.0 ]) |> PipeItem
-        | 'S' -> StartItem
-        | _ -> Unknown
-
-    let isPipe =
-        function
-        | '|'
-        | '-'
-        | 'J'
-        | '7'
-        | 'F'
-        | 'L' -> true
-        | _ -> false
-
     let canEnter direction (Pipe(a, b)) = direction = a || direction = b
 
-    let enter cursor =
+    let findOutDirection inDirection =
         function
-        | Pipe(inDirection, other)
-        | Pipe(other, inDirection) when inDirection = cursor.direction ->
-            { direction = other |> Vector.neg
-              location = cursor.location + inDirection }
+        | Pipe(i, other)
+        | Pipe(other, i) when i = inDirection -> other |> Vector.neg
         | _ -> failwith "can't traverse pipe in this direction"
 
+let walkLoop lines =
+
+    let initialLocation = lines |> Grid.findStart
+
+    let initialDirection =
+        Grid.allDirections
+        |> Seq.find (fun direction ->
+            lines
+            |> Grid.tryItemAt (initialLocation + direction)
+            |> Option.bind (function
+                | PipeItem p -> Some p
+                | _ -> None)
+            |> Option.map (Pipe.canEnter direction)
+            |> Option.defaultValue false)
+
+    let initialCursor =
+        { position = initialLocation
+          direction = initialDirection }
+
+    let nextCursors =
+        initialCursor
+        |> List.unfold (fun cursor ->
+            let nextPosition = cursor |> Cursor.forwardPosition
+
+            match lines |> Grid.itemAt nextPosition with
+            | StartItem -> None
+            | PipeItem pipe ->
+                let nextCursor =
+                    { position = nextPosition
+                      direction = pipe |> Pipe.findOutDirection cursor.direction }
+
+                Some(nextCursor, nextCursor)
+
+            | _ -> failwith "unexpected break in loop")
+
+    initialCursor :: nextCursors
+
 let part1 lines =
-    let start = lines |> Grid.findStart
+    let allCursors = walkLoop lines
+    (allCursors |> List.length) / 2
 
-    let possibleDirections =
-        [ vector [| 1.0; 0.0 |]
-          vector [| -1.0; 0.0 |]
-          vector [| 0.0; 1.0 |]
-          vector [| 0.0; 1.0 |] ]
-        |> List.filter (fun dir ->
-            match lines |> Grid.charAt (start + dir) |> Pipe.fromChar with
-            | PipeItem pipe -> pipe |> Pipe.canEnter dir
-            | _ -> false)
+let part2 lines =
 
-    let cursor =
-        { location = start
-          direction = possibleDirections[0] }
+    let allCursors = walkLoop lines
 
-    let rec loopSize i cursor =
-        match lines |> Grid.charAt (cursor.location + cursor.direction) |> Pipe.fromChar with
-        | PipeItem nextPipe -> loopSize (i + 1) (Pipe.enter cursor nextPipe)
-        | StartItem -> i
-        | _ -> failwith "unexpected break in loop"
+    let loopPositions = allCursors |> Seq.map _.position |> Set.ofSeq
 
-    (loopSize 1 cursor) / 2
+    let rec expand allTiles newLocations =
 
+        let newTiles =
+            newLocations
+            |> Seq.filter (fun loc ->
+                (lines |> Grid.tryCharAt loc).IsSome
+                && (loopPositions |> Set.contains loc |> not)
+                && (allTiles |> Set.contains loc |> not))
+            |> Set.ofSeq
 
-let part2 lines = 0
+        if (newTiles.Count = 0) then
+            allTiles
+        else
+            expand
+                (allTiles |> Set.union newTiles)
+                ((Grid.allDirections, newTiles)
+                 ||> Seq.allPairs
+                 |> Seq.map (fun (direction, newTile) -> newTile + direction))
+
+    let cursorsWithPreviousDirections =
+        allCursors
+        |> Seq.pairwise
+        |> Seq.map (fun (a, b) -> { b with direction = a.direction })
+        |> Seq.toList
+
+    let leftPos, rightPos =
+        (allCursors @ cursorsWithPreviousDirections)
+        |> List.map Cursor.leftRightPositions
+        |> List.unzip
+        |> TupleEx.map (fun list -> expand Set.empty list)
+
+    lines
+    |> Array.iteri (fun y line ->
+        line
+        |> Seq.iteri (fun x char ->
+            let pos = vector [| x; y |]
+
+            Spectre.Console.AnsiConsole.Markup(
+                if (leftPos |> Set.contains pos) then
+                    $"[red]{char}[/]"
+                elif (rightPos |> Set.contains pos) then
+                    $"[lime]{char}[/]"
+                elif (loopPositions |> Set.contains pos) then
+                    $"[yellow on blue]{char}[/]"
+                else
+                    $"[black on pink1]{char}[/]"
+            ))
+
+        printfn "")
+
+    let inner =
+        [ leftPos; rightPos ]
+        |> Seq.filter (fun set ->
+            set
+            |> Set.exists (fun vec -> (Vector.get vec 0) = 0.0 || (Vector.get vec 1) = 0.0)
+            |> not)
+        |> Seq.exactlyOne
+        
+    inner.Count
 
 let run = runReadAllLines part1 part2
 
@@ -113,3 +211,19 @@ module Tests =
 
     [<Fact>]
     let ``part 1 example 2`` () = part1 example2 =! 8
+
+
+    let example3 =
+        [| "FF7FSF7F7F7F7F7F---7"
+           "L|LJ||||||||||||F--J"
+           "FL-7LJLJ||||||LJL-77"
+           "F--JF--7||LJLJ7F7FJ-"
+           "L---JF-JLJ.||-FJLJJ7"
+           "|F|F-JF---7F7-L7L|7|"
+           "|FFJF7L7F-JF7|JL---7"
+           "7-L-JL7||F7|L7F-7F7|"
+           "L.L7LFJ|||||FJL7||LJ"
+           "L7JLJL-JLJLJL--JLJ.L" |]
+
+    [<Fact>]
+    let ``part 2 example 2`` () = part2 example3 =! 10
