30a8ea1307e337f618d03fa3ce2ca3e507944aac
andi0b <andix25@gmail.com>
2023-12-13 13:00:47 +0100

    Day 13

--- Day13.fs
+++ Day13.fs
@@ -13,7 +13,6 @@ module MirrorPoint =
         | Row r -> r * 100
 
 type Grid(input) =
-
     let matrix =
         let lines = input |> StringEx.splitSs [| "\r\n"; "\n" |]
         let rows = lines.Length
@@ -26,59 +25,46 @@ type Grid(input) =
 
     let cols = [| 0 .. matrix.NumCols - 1 |] |> Array.map (Matrix.getCol matrix)
 
+    let findMirrorPoint comparer =
+        seq {
+            for r in 1 .. matrix.NumRows - 1 do
+                if comparer rows r then
+                    yield Row r
+
+            for c in 1 .. matrix.NumCols - 1 do
+                if comparer cols c then
+                    yield Col c
+        }
+        |> Seq.exactlyOne
 
     member self.MirrorPoint =
         let isMirrorAt (array: 'a array) idx =
             let first, second = (array[.. idx - 1], array[idx..])
             (first |> Seq.rev, second) ||> Seq.forall2 (=)
 
-        seq {
-            for r in 1 .. matrix.NumRows - 1 do
-                if isMirrorAt rows r then
-                    yield Row r
-
-            for c in 1 .. matrix.NumCols - 1 do
-                if isMirrorAt cols c then
-                    yield Col c
-        }
-        |> Seq.exactlyOne
+        findMirrorPoint isMirrorAt
 
     member self.UnsmugedMirrorPoint =
-        let inline mirrorAtDifference (array: vector array) idx =
+        let isMirrorWithExactlyOneSmudge (array: vector array) idx =
             let first, second = (array[.. idx - 1], array[idx..])
 
             (first |> Seq.rev, second)
             ||> Seq.map2 (fun a b -> (a - b) |> Vector.map abs |> Vector.sum)
-            |> Seq.sum
+            |> Seq.sum = 1.0
 
-        seq {
-            for r in 1 .. matrix.NumRows - 1 do
-                if (mirrorAtDifference rows r) = 1.0 then
-                    yield Row r
-
-            for c in 1 .. matrix.NumCols - 1 do
-                if (mirrorAtDifference cols c) = 1.0 then
-                    yield Col c
-        }
-        |> Seq.exactlyOne
+        findMirrorPoint isMirrorWithExactlyOneSmudge
 
     static member ParseMany input =
         let parts = input |> StringEx.splitSs [| "\r\n\r\n"; "\n\n" |]
         parts |> Array.map Grid
 
-
-
-let part1 input =
+let solve mapper input =
     let grids = Grid.ParseMany input
 
-    grids |> Seq.map (fun g -> g.MirrorPoint |> MirrorPoint.value) |> Seq.sum
+    grids |> Seq.map (fun g -> g |> mapper |> MirrorPoint.value) |> Seq.sum
 
-let part2 input =
-    let grids = Grid.ParseMany input
-
-    grids |> Seq.map (fun g -> g.UnsmugedMirrorPoint |> MirrorPoint.value) |> Seq.sum
-
-
+let part1 = solve _.MirrorPoint
+let part2 = solve _.UnsmugedMirrorPoint
 let run = runReadAllText part1 part2
 
 module Tests =
@@ -104,6 +90,6 @@ module Tests =
 
     [<Fact>]
     let ``part 1 example`` () = part1 example =! 405
-    
+
     [<Fact>]
     let ``part 2 example`` () = part2 example =! 400
